e583b7632ed1ff341c0c5833c3ff21131de90b5e
Nakul Bhardwaj <117837100+nakulbh@users.noreply.github.com>
2023-10-27 22:01:52 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,39 +1,37 @@
-
 import React from "react";
-import styles from "@/styles/about.module.css"
+import Image from 'next/image'; // Import Image from next/image
+import styles from "@/styles/about.module.css";
 import Cardclient from "@/components/aboutCard";
 
-const page = () => {
+const Page = () => { // Renamed the function to start with an uppercase letter
   return (
     <div className={styles.maincard}>
-    <div className={styles.image}>
-    <img src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg" alt="about"/>
+      <div className={styles.image}>
+        <Image // Use Image component for image optimization
+          src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg"
+          alt="about"
+          width={500} // Adjust width and height as needed
+          height={300}
+        />
+      </div>
+      <div className={styles.heading}>
+        <h1>ABOUT US</h1>
+      </div>
+      <hr />
+
+      <div className={styles.whowe}>
+        <h1>WHO WE ARE</h1>
+        <p>Empire technology Web Services is a company committed to providing world-class services that accelerate the growth and success of their clients. Here at Empires tech we are committed to providing effective solutions with our diverse range of services including Software Development, Payment Gateway Solutions, Web Design And Development, Digital Marketing, Search Engine Optimization, and Android Apps. The company creates an environment for its employees, a team of experts across various fields who utilize their expertise and vast domain knowledge to accelerate the growth and success of their clients, and who have enough space to work independently.</p>
+      </div>
+      <hr />
+
+      <div className={styles.cards}>
+        <Cardclient name="Satyam Samele" position="CEO" />
+        <Cardclient name="Amit Singh" position="President" />
+        <Cardclient name="Sachin Chaturvedi" position="CFO" />
+      </div>
     </div>
-    <div className={styles.heading}>
-    <h1>ABOUT US</h1>
-    </div>
-    <hr />
-
-
-    <div className={styles.whowe}>
-      <h1>WHO WE ARE</h1>
-      <p>Empire technology Web Services is a company committed to providing world-class services that accelerate the growth and success of their clients. Here at Empires tech we are committed to providing effective solutions with our diverse range of services including Software Development, Payment Gateway Solutions, Web Design And Development, Digital Marketing, Search Engine Optimization,  and Android Apps. The company creates an environment for its employees, a team of experts across various fields who utilize their expertise and vast domain knowledge to accelerate the growth and success of their clients, and who have enough space to work independently.</p>
-    </div>
-    <hr />
-
-
-    <div className={styles.cards}>
-    <Cardclient name="Satyam Samele" position="CEO"></Cardclient>
-    <Cardclient name="Amit Singh" position="President"></Cardclient>
-    <Cardclient name="Sachin Chaturvedi" position="CFO"></Cardclient>
-   
-    </div>
-   
- 
-</div>
-
-   
   );
 };
 
-export default page;
+export default Page;
